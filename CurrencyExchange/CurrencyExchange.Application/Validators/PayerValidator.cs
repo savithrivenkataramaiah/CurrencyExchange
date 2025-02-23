ddcc342188e23cfa2c6db40656936406ffea3b77
savithrivenkataramaiah <savithri.rkra@gmail.com>
2025-02-23 18:37:29 +1100

    Update PayerValidator.cs

--- CurrencyExchange/CurrencyExchange.Application/Validators/PayerValidator.cs
+++ CurrencyExchange/CurrencyExchange.Application/Validators/PayerValidator.cs
@@ -1,4 +1,5 @@
-﻿using CurrencyExchange.DTO;
+using CurrencyExchange.Core.Common;
+using CurrencyExchange.DTO;
 using FluentValidation;
 
 namespace CurrencyExchange.Application.Validators
@@ -12,13 +13,15 @@ namespace CurrencyExchange.Application.Validators
                 .DependentRules(() =>
                 {
                     RuleFor(x => x.Name).MinimumLength(2).WithMessage("Payer Name must have minimum of 2 characters")
-                    .MaximumLength(40).WithMessage("Payer Name can have a maximum of 40 characters");
+                    .MaximumLength(40).WithMessage("Payer Name can have a maximum of 40 characters")
+                    .Matches(Constants.RegExForName).WithMessage("Invalid Characters for Name. Allowed characters are: A-Za-z'- and a space");
                 });
             RuleFor(x => x.TransferReason).NotEmpty().WithMessage("Transfer Reason cannot be empty.")
                 .DependentRules(() =>
                 {
                     RuleFor(x => x.TransferReason).MinimumLength(3).WithMessage("Transfer Reason must have minimum of 3 characters")
-                    .MaximumLength(30).WithMessage("Transfer Reason can have a maximum of 30 characters");
+                    .MaximumLength(30).WithMessage("Transfer Reason can have a maximum of 30 characters")
+                    .Matches(Constants.RegExForTransferReason).WithMessage("Invalid Characters for Transfer Reason. Allowed characters are: A-Za-z and a space");
                 });
         }
     }
