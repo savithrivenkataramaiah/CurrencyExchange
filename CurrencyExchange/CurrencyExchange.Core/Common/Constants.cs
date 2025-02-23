e41bfefd50a7b9f3365303d95df2ba8c93840545
savithrivenkataramaiah <savithri.rkra@gmail.com>
2025-02-23 18:36:25 +1100

    Update Constants.cs

--- CurrencyExchange/CurrencyExchange.Core/Common/Constants.cs
+++ CurrencyExchange/CurrencyExchange.Core/Common/Constants.cs
@@ -1,8 +1,12 @@
-﻿namespace CurrencyExchange.Core.Common
+namespace CurrencyExchange.Core.Common
 {
     public static class Constants
     {
         public static readonly string[] SellCurrencies = ["AUD", "USD", "EUR"];
         public static readonly string[] BuyCurrencies = ["USD", "INR", "PHP"];
+        public static readonly string RegExForName = @"^[A-Za-z\s-']*$";
+        public static readonly string RegExForTransferReason = @"^[A-Za-z\s]*$";
+        public static readonly string RegExForBankName = @"^[A-Za-z\s]*$";
+        public static readonly string RegExDigits = @"^[0-9]*$";
     }
 }
