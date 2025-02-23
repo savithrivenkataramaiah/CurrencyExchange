81841c1f8bdac5dab4e2c0aa24f7e49b889ea391
savithrivenkataramaiah <savithri.rkra@gmail.com>
2025-02-23 18:38:01 +1100

    Update RecipientValidator.cs

--- CurrencyExchange/CurrencyExchange.Application/Validators/RecipientValidator.cs
+++ CurrencyExchange/CurrencyExchange.Application/Validators/RecipientValidator.cs
@@ -1,4 +1,5 @@
-﻿using CurrencyExchange.DTO;
+using CurrencyExchange.Core.Common;
+using CurrencyExchange.DTO;
 using FluentValidation;
 
 namespace CurrencyExchange.Application.Validators
@@ -11,24 +12,28 @@ namespace CurrencyExchange.Application.Validators
                 .DependentRules(() =>
                 {
                     RuleFor(x => x.Name).MinimumLength(2).WithMessage("Recipient Name must have minimum of 2 characters")
-                    .MaximumLength(40).WithMessage("Recipient Name can have a maximum of 40 characters");
+                    .MaximumLength(40).WithMessage("Recipient Name can have a maximum of 40 characters")
+                    .Matches(Constants.RegExForName).WithMessage("Invalid Characters for Name. Allowed characters are: A-Za-z'- and a space");
                 });
             RuleFor(x => x.AccountNumber).NotEmpty().WithMessage("Account Number cannot be empty.")
                 .DependentRules(() =>
                 {
                     RuleFor(x => x.AccountNumber).MinimumLength(6).WithMessage("AccountNumber must have minimum of 6 characters")
-                    .MaximumLength(12).WithMessage("Account Number can have a maximum of 12 characters");
+                    .MaximumLength(12).WithMessage("Account Number can have a maximum of 12 characters")
+                    .Matches(Constants.RegExDigits).WithMessage("Only number 0-9 are allowed for Account Number");
                 });
             RuleFor(x => x.BankName).NotEmpty().WithMessage("Bank Name cannot be empty.")
                 .DependentRules(() =>
                 {
                     RuleFor(x => x.BankName).MinimumLength(3).WithMessage("Bank Name must have minimum of 3 characters")
-                    .MaximumLength(50).WithMessage("Bank Name can have a maximum of 50 characters");
+                    .MaximumLength(50).WithMessage("Bank Name can have a maximum of 50 characters")
+                    .Matches(Constants.RegExForBankName).WithMessage("Invalid Characters for Bank Name. Allowed characters are: A-Za-z and a space");
                 });
             RuleFor(x => x.BankCode).NotEmpty().WithMessage("Bank Code cannot be empty.")
                 .DependentRules(() =>
                 {
-                    RuleFor(x => x.BankCode).Length(6).WithMessage("Bank Code must have 6 characters");
+                    RuleFor(x => x.BankCode).Length(6).WithMessage("Bank Code must have 6 characters")
+                    .Matches(Constants.RegExDigits).WithMessage("Only number 0-9 are allowed for Bank Code");
                 });
         }
     }
